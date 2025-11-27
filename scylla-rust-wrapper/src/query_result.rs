84469d92390270d38b62f38297405814886bc618
Karol Baryła <karol.baryla@scylladb.com>
2025-11-27 18:44:26 +0100

    Fix clippy complaint
    Clippy noticed that in AttachError enum variants have very different
    sizes. The error variant contents were later put into Arc anyway, so the
    solution is to just to that earlier.

--- scylla-rust-wrapper/src/query_result.rs
+++ scylla-rust-wrapper/src/query_result.rs
@@ -227,7 +227,6 @@ impl<'result> CassRow<'result> {
 mod row_with_self_borrowed_result_data {
     use std::sync::Arc;
 
-    use scylla::errors::DeserializationError;
     use yoke::{Yoke, Yokeable};
 
     use crate::cass_error::CassErrorResult;
@@ -261,12 +260,12 @@ mod row_with_self_borrowed_result_data {
             raw_rows_and_metadata: Arc<CassRowsResultSharedData>,
         ) -> Result<Option<Self>, Arc<CassErrorResult>> {
             enum AttachError {
-                CassErrorResult(CassErrorResult),
+                CassErrorResult(Arc<CassErrorResult>),
                 NoRows,
             }
             impl From<CassErrorResult> for AttachError {
                 fn from(err: CassErrorResult) -> Self {
-                    AttachError::CassErrorResult(err)
+                    AttachError::CassErrorResult(Arc::new(err))
                 }
             }
 
@@ -286,8 +285,8 @@ mod row_with_self_borrowed_result_data {
                         .and_then(|raw_row| CassRow::from_raw_row_and_metadata(raw_row, metadata));
 
                     let row = row_result
-                        .map_err(DeserializationError::into)
-                        .map_err(AttachError::CassErrorResult)?;
+                        .map_err(CassErrorResult::from)
+                        .map_err(AttachError::from)?;
 
                     Ok(CassRowWrapper(row))
                 },
@@ -296,7 +295,7 @@ mod row_with_self_borrowed_result_data {
             match yoke_result {
                 Ok(yoke) => Ok(Some(Self(yoke))),
                 Err(AttachError::NoRows) => Ok(None),
-                Err(AttachError::CassErrorResult(err)) => Err(Arc::new(err)),
+                Err(AttachError::CassErrorResult(err)) => Err(err),
             }
         }
 
