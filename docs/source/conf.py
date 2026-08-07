af6e7a1eec104b63ae0187defbd043e4a81c09a2
Wojciech Przytuła <wojciech.przytula@scylladb.com>
2026-08-07 13:28:03 +0200

    cargo: bump cpp-rs-driver to 1.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,16 +15,16 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Global variables
 
 # Builds documentation for the following tags and branches.
-TAGS = ["v1.0.0", "v1.0.1", "v1.1.0"]
+TAGS = ["v1.0.0", "v1.0.1", "v1.1.0", "v1.1.1"]
 BRANCHES = [
     "master",
 ]
 # Sets the latest version.
-LATEST_VERSION = "v1.1.0"
+LATEST_VERSION = "v1.1.1"
 # Set which versions are not released yet.
 UNSTABLE_VERSIONS = ["master"]
 # Set which versions are deprecated
-DEPRECATED_VERSIONS = ["v1.0.0", "v1.0.1"]
+DEPRECATED_VERSIONS = ["v1.0.0", "v1.0.1", "v1.1.0"]
 # Sets custom build.
 FLAGS = ["theme"]
 
